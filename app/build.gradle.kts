b0c58c2c2066624b814b116fd63642cee562097c
tienpy <tienpy190congviec@gmail.com>
2026-09-10 08:52:21 +0700

    Bump version to 1.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.mrtien.mbbankguard15"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.5.1"
+        versionCode = 12
+        versionName = "1.5.2"
     }
 
     signingConfigs {
