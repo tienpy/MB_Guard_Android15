033b915d6c1a79f9eba1520e4201f173f040fcce
tienpy <tienpy190congviec@gmail.com>
2026-09-10 11:01:57 +0700

    Gesture Actions v2: bump to 2.0.0

--- shakeactions/build.gradle.kts
+++ shakeactions/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.mrtien.tienshakeactions"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "2.0.0"
     }
     signingConfigs {
         create("releaseKey") {
