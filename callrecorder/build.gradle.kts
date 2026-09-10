4ef17cad8768ce17d9b8163ff8fd30358376ad41
tienpy <tienpy190congviec@gmail.com>
2026-09-10 13:11:35 +0700

    Call Recorder: bump to v1.2.0

--- callrecorder/build.gradle.kts
+++ callrecorder/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.mrtien.tiencallrecorder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
     }
     signingConfigs {
         create("releaseKey") {
