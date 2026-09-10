c16d2f6a833f35e3c758d327f5dcfc20c48be341
tienpy <tienpy190congviec@gmail.com>
2026-09-10 10:08:21 +0700

    Shake Actions: add Gradle module

---
+++ shakeactions/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+}
+android {
+    namespace = "com.mrtien.tienshakeactions"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.mrtien.tienshakeactions"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+    signingConfigs {
+        create("releaseKey") {
+            storeFile = rootProject.file("signing/mbguard15.p12")
+            storePassword = "MBGuard15_2026"
+            keyAlias = "mbguard15"
+            keyPassword = "MBGuard15_2026"
+            storeType = "PKCS12"
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("releaseKey")
+            isMinifyEnabled = false
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("releaseKey")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
